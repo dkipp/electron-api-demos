4945a82619d3993df7fa02247e068f55ae7d050e
dkipp <mail@daniel-kipp.de>
2018-07-25 22:22:08 +0200

    zoom = 0.25

--- renderer-process/media/pdf-pages.js
+++ renderer-process/media/pdf-pages.js
@@ -1,34 +1,33 @@
-const { ipcRenderer } = require('electron')
-const pdfjsLibPages = require('pdfjs-dist')
+const { ipcRenderer } = require("electron");
+const pdfjsLibPages = require("pdfjs-dist");
 
-pdfjsLibPages.GlobalWorkerOptions.workerSrc = './node_modules/pdfjs-dist/build/pdf.worker.js'
+pdfjsLibPages.GlobalWorkerOptions.workerSrc =
+  "./node_modules/pdfjs-dist/build/pdf.worker.js";
 
-const allPDFBtn = document.getElementById('all-page-demo-button')
-const canvasContainer = document.getElementById('all-page-pdf-canvases')
+const allPDFBtn = document.getElementById("all-page-demo-button");
+const canvasContainer = document.getElementById("all-page-pdf-canvases");
 let pdfdoc = null;
-let pdfPath = 'assets/pdf/das-kleine-schwarze-naehen-data.pdf'
+let pdfPath = "assets/pdf/das-kleine-schwarze-naehen-data.pdf";
 
-allPDFBtn.addEventListener('click', (event) => {
-
-  pdfjsLibPages.getDocument(pdfPath).then((pdf) => {
-
+allPDFBtn.addEventListener("click", event => {
+  pdfjsLibPages.getDocument(pdfPath).then(pdf => {
     pdfdoc = pdf;
 
-    for(page = 1; page <= pdfdoc.numPages; page++) {
-      canvas = document.createElement("canvas")
-      canvas.className = 'pdf-page-canvas'
-      canvasContainer.appendChild(canvas)
-      renderPage(page, canvas)
-    }    
-  })
-})
+    for (page = 1; page <= pdfdoc.numPages; page++) {
+      canvas = document.createElement("canvas");
+      canvas.className = "pdf-page-canvas";
+      canvasContainer.appendChild(canvas);
+      renderPage(page, canvas);
+    }
+  });
+});
 
 function renderPage(pageNumber, canvas) {
-  pdfdoc.getPage(pageNumber).then( (page) => {
-    let scale = .1;
-    viewport = page.getViewport(scale)
-    canvas.height = viewport.height
-    canvas.width = viewport.width
-    page.render({canvasContext: canvas.getContext('2d'), viewport: viewport})
-  })
+  pdfdoc.getPage(pageNumber).then(page => {
+    let scale = 0.25;
+    viewport = page.getViewport(scale);
+    canvas.height = viewport.height;
+    canvas.width = viewport.width;
+    page.render({ canvasContext: canvas.getContext("2d"), viewport: viewport });
+  });
 }
